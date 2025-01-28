c7aa066396709dcde6a0025d4d11aca524824e3d
Yousan_O <yousan@l2tp.org>
2025-01-28 10:47:48 +0900

    fix: modify seed.sql to use transaction instead of disabling triggers

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,8 +1,5 @@
--- Disable triggers temporarily
-ALTER TABLE auth.users DISABLE TRIGGER ALL;
-ALTER TABLE profiles DISABLE TRIGGER ALL;
-ALTER TABLE lesson_levels DISABLE TRIGGER ALL;
-ALTER TABLE lessons DISABLE TRIGGER ALL;
+-- Start transaction
+BEGIN;
 
 -- Import test users
 COPY auth.users (id, email, role)
@@ -24,8 +21,5 @@ COPY lessons (id, level_id, name, description, duration, max_participants)
 FROM '/seed-data/lessons.csv'
 WITH (FORMAT csv, HEADER true);
 
--- Re-enable triggers
-ALTER TABLE auth.users ENABLE TRIGGER ALL;
-ALTER TABLE profiles ENABLE TRIGGER ALL;
-ALTER TABLE lesson_levels ENABLE TRIGGER ALL;
-ALTER TABLE lessons ENABLE TRIGGER ALL; 
+-- Commit transaction
+COMMIT; 
